9bb2df2912c7b674e93f7f3b5c8af21384aa77eb
Riccardo Gulin <riccardo@nucleode.com>
2022-02-15 10:34:36 +0100

    Adds basic server; adds mongoose schema and model

---
+++ src/services/users/schema.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose"
+
+const { Schema, model } = mongoose
+
+const userSchema = new Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true },
+    dateOfBirth: { type: Date, required: true },
+    age: { type: Number, min: 18, max: 65, required: true },
+    professions: [String],
+  },
+  {
+    timestamps: true, // adds and manages automatically createdAt and updatedAt fields
+  }
+)
+
+export default model("User", userSchema) // this model is now automatically linked to the "users" collection, if the collection is not there it will be automatically created
